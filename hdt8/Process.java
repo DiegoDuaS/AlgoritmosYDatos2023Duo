a603f0df12d11b5a5de7fc67c7e074ce960dab0e
sofia g <110646163+sofigo1010@users.noreply.github.com>
2023-04-02 22:02:47 -0600

    proceso

--- hdt8/Process.java
+++ hdt8/Process.java
@@ -1,3 +1,52 @@
-public class Process {
+/**
+* Tarea: Hoja de trabajo 8
+* Autores: Sofia Garcia, Diego Duarte
+* Fecha: 02/04/2023
+*/
+
+public class Process implements Comparable<Process>{
+    String process = "";
+    String username;
+    int nice = 0; 
+    int priority;
+
+    /**
+     * @param processName
+     * @param userName
+     * @param niceValue
+     * Constructor de la clase, se necesita un nombre de proceso, seguido del usuario, valor de nice y la prioridad que es calculada con nice   
+     * */
+    public Process(String processName, String userName,Integer niceValue){
+        this.process = processName;
+        this.username = userName;
+        this.nice = niceValue;
+        Priority(niceValue);
+    }
+
+    /**
+     * @param niceValue
+     * Prioridad es 120 + el valor nice
+     */
+    private void Priority(Integer niceValue){
+        priority = 120+niceValue;
+    }
+
+    public Integer getPriority(){
+        return priority;
+    }
+    public int getNice() {
+        return nice;
+    }
+    public String getProcess() {
+        return process;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public int compareTo(Process o) {
+        return this.getPriority().compareTo(o.getPriority());
+    }
     
 }
